5cf12d3d85e1b6d3e42ae50e9c4a079baa468b0b
Gilberto Cortez <me@gilbertocortez.com>
2020-01-26 22:29:18 -0800

    index notes

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Load App component into Browser DOM
 ReactDOM.render(<App />, document.getElementById('root'));
